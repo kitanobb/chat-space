8d69454303809259bbeef113f5a26ff5c2dd5043
kitanobb <mkitanobb@gmail.com>
2020-05-09 18:44:55 +0900

    feat updateメソッドの成否で処理を分岐させる

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,11 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
+    if @group.update(group_params)
+
+    else
+
+    end
   end
   
   private
