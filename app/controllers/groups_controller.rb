5446b154a8e6af6bbc80e79cda1386f1edb09671
kitanobb <mkitanobb@gmail.com>
2020-05-09 18:24:35 +0900

    feat saveメソッドが成功したら、redirect_toでrootを表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-
+      redirect_to root_path, notice: 'グループを作成しました'
     else
 
     end
