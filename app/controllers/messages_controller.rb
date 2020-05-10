03ba28ff6986d490e2b3850c23e89d937abdc933
kitanobb <mkitanobb@gmail.com>
2020-05-10 10:19:35 +0900

    feat メッセージの保存が成功したら、redirect_toで該当groupのメッセージ一覧を表示する

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,9 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_paramas)
     if @message.save
-
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
-
+      
     end
   end
 
