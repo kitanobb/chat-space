c26fd0fe81dde8bb585a9bdb04fc0adc28ce735b
kitanobb <mkitanobb@gmail.com>
2020-05-10 10:01:53 +0900

    feat before_actionで、groupモデルから該当のIDを見つけて変数groupに代入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,15 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+  
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
 
   def create
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
