a1cdc9c60e3ba18dd29e893cc172b09237835879
kitanobb <mkitanobb@gmail.com>
2020-05-10 10:14:48 +0900

    feat messageモデルのbodyカラムとimageカラムに、ログイン済みのuserIDをマージして、保存する許可を設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,6 +13,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
   def set_group
