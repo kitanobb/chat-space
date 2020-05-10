d41f711c706862d1d3eef0317a334801b7bb844c
kitanobb <mkitanobb@gmail.com>
2020-05-10 16:42:30 +0900

    feat メッセージのテキストがあるかを確認して、画像のみの場合と処理を分岐させる

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,10 +7,13 @@ class Group < ApplicationRecord
 
   def show_last_message
     if (last_message = messages.last).present?
-      
+      if last_message.body?
+        last_message.body
+      else
+        '画像が投稿されています'
+      end
     else
       'まだメッセージがありません'
     end
   end
-  
 end
