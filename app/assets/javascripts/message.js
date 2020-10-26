181810c63f02b10f30b30e34210aa327e512cc0d
kitanobb <mkitanobb@gmail.com>
2020-10-26 12:05:22 +0900

    非同期通信機能を実装
    フォームが送信されたらイベントを発火させmessages#createを動くようにする。
    コントローラでメッセージを保存しrespond_toでJSONの処理を分ける。
    jbuilderを使用して作成したメッセージをJSONで返す。
    帰ってきたJSONをdoneメソッドで受け取ってHTMLを作成、メッセージ画面の一番下に追加する。
    送信後にスクロールさせる。
    送信ボタンを連続で使えるように。
    失敗した場合にアラートを出す。
    日付表示を日本時間にする。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,62 @@
-$(function(){
-  
-})
+$(function(){ 
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<article class="messages__unit">
+          <div class="messages__username">
+            ${message.user_name}
+            <div class="messages__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="messages__text">
+            <p class="messages__text">
+              ${message.body}
+            </p>
+            <img src=${message.image} class="lower-message__image" >
+          </div>
+        </article>`
+      return html;
+    } else {
+      var html =
+        `<article class="messages__unit">
+          <div class="messages__username">
+            ${message.user_name}
+            <div class="messages__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="messages__text">
+            <p class="messages__text">
+              ${message.body}
+            </p>
+          </div>
+        </article>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.post-form__snd-btn').prop('disabled', false);
+        $('form')[0].reset();
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+        $('.post-form__snd-btn').prop('disabled', false);
+      });
+  })
+});
